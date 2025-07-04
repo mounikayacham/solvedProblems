5ae8b4c82fd10408299470b26dbc2b04596d6f16
mounikayacham <142496630+mounikayacham@users.noreply.github.com>
2025-07-04 22:04:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Multiply two strings/multiply-two-strings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def multiplyStrings(self, s1, s2):
+        # code here
+        # return the product string
+        a=int(s1)
+        b=int(s2)
+        res=a*b
+        return str(res)
