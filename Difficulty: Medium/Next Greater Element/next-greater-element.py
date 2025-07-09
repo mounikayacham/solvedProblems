98ffd80d16b879cf37a7ba67b0bb39cfedc41606
mounikayacham <142496630+mounikayacham@users.noreply.github.com>
2025-07-09 21:38:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element/next-greater-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nextLargerElement(self, arr):
+        # code here
+        res=[-1]*(len(arr))
+        stack=[]
+        for i in range(len(arr)-1,-1,-1):
+            while stack and stack[-1]<=arr[i]:
+                stack.pop()
+            if stack:
+                res[i]=stack[-1]
+            stack.append(arr[i])
+        return res
