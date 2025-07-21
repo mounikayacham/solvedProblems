bef0f13e19f0efcfd3e9e0fce36025a2b39dad14
mounikayacham <142496630+mounikayacham@users.noreply.github.com>
2025-07-21 22:02:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def circularSubarraySum(self, arr):
+        # code here
+        totalsum=arr[0]
+        minsum=arr[0]
+        maxsum=arr[0]
+        Cmaxsum=0
+        Cminsum=0
+        for i in range(1,len(arr)):
+            Cmaxsum=max(Cmaxsum+arr[i],arr[i])
+            maxsum=max(maxsum,Cmaxsum)
+            Cminsum=min(Cminsum+arr[i],arr[i])
+            minsum=min(minsum,Cminsum)
+            totalsum+=arr[i]
+        normalsum=maxsum
+        circularsum=totalsum-minsum
+        if totalsum==minsum:
+            return normalsum
+        return max(normalsum, circularsum)
+        #print(sol.circularSubarraySum(arr))    
+        # code here
